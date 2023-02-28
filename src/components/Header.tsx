98381268a22332ef63ca2efed54ef3e6dce413b6
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2023-02-27 21:26:29 -0600

    Index 100%?
    Index terminado al parecer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,14 @@ function Header(){
         <nav>
             <img src="/src/assets/LogoFlowey.png" alt="Logotipo" />
             <div className='Op1'>
-                <a href="#">Inicio</a>
+                <a href="index.html">Inicio</a>
                 <a href="#">Productos</a>
                 <a href="#">Nosotros</a>
             </div>
 
             <div className='Op2'>
-                <a href="#">Registrarse</a>
-                <a href="#">Iniciar sesión</a>
+                <a href="register.html">Registrarse</a>
+                <a href="login.html">Iniciar sesión</a>
             </div>
         </nav>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './login.css'
+import './register.css'
 import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
